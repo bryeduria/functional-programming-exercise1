efe02cf28410704651571153587bf73038446576
Bryan Eduria <bryan.eduria@whitecloak.com>
2019-08-28 15:13:40 +0800

    Exercise #1

--- src/com/whitecloak/ItemSales.java
+++ src/com/whitecloak/ItemSales.java
@@ -1,12 +1,22 @@
 package com.whitecloak;
 
-import com.sun.source.tree.Tree;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ItemSales {
 
     public static void main(String[] args) {
+        Set<String> itemsSold = new TreeSet<>();
+        try (Stream<String> read = Files.lines(Paths.get("data/cebu.csv"))) {
+            itemsSold = read.collect(Collectors.toSet());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
-
-    public static Tree<>
 }
